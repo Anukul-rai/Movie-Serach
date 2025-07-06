37bd95bd8d673df971a48c69ce51ac61f1c9b525
anukul rai <anukul.rau@gmail.com>
2025-07-07 00:33:54 +0545

    some updates

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 function MovieCard() {
   return (
     <div className='flex flex-col items-center justify-center'>
-      <img src="/POWER.png" alt="image" className='h-40 m-3 p-2' />
-      <h1 className='font-semibold'>Title of the Movie</h1>
+      <div className='border mt-5 px-2'>
+      <img src="/POWER.png" alt="image" className='h-40 m-3 border border-yellow-700' />
+      <h1 className='font-semibold text-xl text-center'>Title of the Movie</h1>
+      <p className=' text-md text-center'>Movie Year</p>
+      </div>
+      
     </div>
   )
 }
